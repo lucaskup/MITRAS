0ce6fafd9a8acbb3a1718b49c3ec9990f59c6c6d
lucaskup <lucas.kup@gmail.com>
2018-05-21 17:51:41 -0300

    Atualizado javascript para permitir criação de mensagens de ambos os lados

--- web/js/eventos.js
+++ web/js/eventos.js
@@ -1,4 +1,20 @@
+tipo = 0
 function enviarMensagem(){
+	texto = document.getElementById( "input" ).value.trim()
+	if(texto != ""){
+		if(tipo == 0){
+			addToMessagePanel(0,texto);
+			tipo = 1;
+		}else{
+			addToMessagePanel(1,texto);
+			tipo = 0;
+		}
+	}
+
+	
+	
+}
+function addToMessagePanel(type, message){
 	//alert("TESTEE");
 	//Usa o dom para criar uma div
 	chat_line = document.createElement( "div" );
@@ -7,32 +23,46 @@ function enviarMensagem(){
 	//usa o dom para criar a div text-wrapper
 	text_wrapper = document.createElement( "div" );
 	text_wrapper.classList.add("text-wrapper") 
-	text_wrapper.classList.add("text-wrapper-right");
+	
 	
 	//usa o dom para criar um texto
 	text_line = document.createElement( "div" );
 	text_line.classList.add("text"); 
-	text_line.classList.add("text-right");
 	
-	texto = document.getElementById( "input" ).value.trim()
-	text_line.innerHTML = texto;
+	
+	text_line.innerHTML = message;
 	
 	text_wrapper.appendChild(text_line);
-	chat_line.appendChild(text_wrapper);
+	
 	
 	avatar = document.createElement( "div" );
 	avatar.classList.add("avatar") 
-	avatar.classList.add("avatar-mitras");
-	chat_line.appendChild(avatar);
+	
+
 	
 	container_block = document.getElementById( 'panel-conversas' );
 	container_block.appendChild( chat_line );
 	document.getElementById( "input" ).value = "";
 	
+	if(type == 0){
+		avatar.classList.add("avatar-mitras");
+		text_wrapper.classList.add("text-wrapper-right");
+		text_line.classList.add("text-right");
+		chat_line.appendChild(text_wrapper);
+		chat_line.appendChild(avatar);
+	}else{
+		avatar.classList.add("avatar-user");
+		text_wrapper.classList.add("text-wrapper-left");
+		text_line.classList.add("text-left");
+		chat_line.appendChild(avatar);
+		chat_line.appendChild(text_wrapper);
+		
+	}
+	
 	
 	chat_line.scrollIntoView();
-	
 }
+
 
 $(document).ready(function() {
 
