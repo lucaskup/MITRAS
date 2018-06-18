d67b3ff54e4a93ee3df397e60ad1cbb76d8a3a2d
Lucas Kupssinsku <lucas.kup@gmail.com>
2018-06-18 13:57:49 -0300

    Retirados system.out.println da instanciação do objeto pois acabavam gerando problema na integração com Prolog

--- Java/SNLPPrologAdapter/src/agilog/snlp/SNLPPrologAdapter.java
+++ Java/SNLPPrologAdapter/src/agilog/snlp/SNLPPrologAdapter.java
@@ -1,5 +1,8 @@
 package agilog.snlp;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -47,18 +50,28 @@ public class SNLPPrologAdapter {
 		return this.ners;
 	}
 	public SNLPPrologAdapter(String text) {
+		// this is your print stream, store the reference
+		//PrintStream err = System.err;
+
+		// now make all writes to the System.err stream silent 
+		//System.setErr(new PrintStream(new OutputStream() {
+			
+	//		@Override
+	//		public void write(int arg0) throws IOException {
+				// TODO Auto-generated method stub
+				
+	//		}
+		//}));
+
+		// YOUR CODE HERE
+
+		// set everything bck to its original state afterwards
+		  
 		
 		// creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER,
 		// parsing, and coreference resolution
 		Properties props = new Properties();
 		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, depparse");
-		//props.setProperty("tokenize.language", "en");
-		//props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz");
-		//props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz");
-		//props.setProperty("depparse.model","edu/stanford/nlp/models/parser/nndep/english_UD.gz");
-		//props.setProperty("coref.mode", "statistical");
-		//props.setProperty("coref.md.type", "dep");
-		//props.setProperty("annotators", "tokenize, ssplit, pos, parse");
 		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 		
 		
@@ -91,7 +104,7 @@ public class SNLPPrologAdapter {
 				String pos = token.get(PartOfSpeechAnnotation.class);
 				// this is the NER label of the token
 				String ne = token.get(NormalizedNamedEntityTagAnnotation.class);
-				System.out.println(ne);
+				//System.out.println(ne);
 				// Monta o predicado das palavras com a extrutura palavra,pos tag e ne tag
 				vetorPOS.add("word(" + word.toLowerCase() + "," + pos.toLowerCase() + "," + "0" + ").");
 				
@@ -103,38 +116,6 @@ public class SNLPPrologAdapter {
 				vetorNER.add("ner(" + word.toLowerCase() + "," + numeroInt[0] + "," + "0" + ").");
 				
 			}
-
-			// this is the parse tree of the current sentence
-//			tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
-//
-//			String arv = tree.toString();
-//			arv = arv.replaceAll("\\(", "parse_tree\\(");
-//			// arv = arv.replaceAll(" ", ",");
-//			String[] a = arv.split(" ");
-//			String arvoreFinal = "";
-//			for (int i1 = 0; i1 < a.length; i1++) {
-//
-//				if (i1 - 1 > 0 && a[i1].contains(")")) {
-//					arvoreFinal += a[i1].replaceFirst("\\)", ",[])");
-//				} else if (i1 == a.length - 1 || a[i1 + 1].startsWith("parse_tree")) {
-//					arvoreFinal += a[i1].trim();
-//				} else {
-//					arvoreFinal += "parse_tree(";
-//				}
-//
-//				if (i1 == a.length - 1) {
-//					arvoreFinal += ".";
-//				} else if (!a[i1 + 1].contains(")")) {
-//					arvoreFinal += ",";
-//					if (!a[i1].endsWith(")")) {
-//						arvoreFinal += "[";
-//					}
-//				}
-//
-//			}
-//			arvoreFinal = arvoreFinal.replaceAll("\\)\\)", ")])");
-//			arvoreFinal = arvoreFinal.replaceAll("\\)\\)", ")])");
-//			this.parseTree = arvoreFinal.toLowerCase();
 
 			// this is the Stanford dependency graph of the current sentence
 			dependencies = sentence.get(EnhancedPlusPlusDependenciesAnnotation.class);
@@ -164,7 +145,7 @@ public class SNLPPrologAdapter {
 			
 			this.basicDependenceGraph = new String[1];
 			this.basicDependenceGraph = grafoDependenciasBasica.toArray(this.basicDependenceGraph);
-			System.out.println(dependencies.edgeListSorted());
+
 		}
 		this.words = vetorPOS.toArray(this.words);
 		this.ners = vetorNER.toArray(this.ners);
@@ -175,6 +156,7 @@ public class SNLPPrologAdapter {
 		// Both sentence and token offsets start at 1!
 		//Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class);
 		limpaApostrofo();
+		//System.setErr(err);
 	}
 
 	private void limpaApostrofo() {
@@ -194,7 +176,7 @@ public class SNLPPrologAdapter {
 		SNLPPrologAdapter n = new SNLPPrologAdapter("to shift Identifier Type up one position");
 		String[] lista = n.getBasicDependenceGraph();
 		for (int i = 0; i < lista.length; i++) {
-			System.out.println(lista[i]);
+			//System.out.println(lista[i]);
 		}
 		
 		
@@ -205,7 +187,7 @@ public class SNLPPrologAdapter {
 //		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 //
 //		// read some text in the text variable
-//		String text = "Lucas is a nice guy and he has 28 years old";
+//		String text = "Lucas is a nice guy and he is 28 years old";
 //
 //		// create an empty Annotation just with the given text
 //		Annotation document = new Annotation(text);
